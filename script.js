3f2894d67608d9c4e00f111b25579109de534276
VictorS357 <victorfransousa357@gmail.com>
2026-04-29 15:52:44 -0300

    fix some styling and make the other links interactive

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const loginContainer = document.querySelector('.js-login-container');
-const createAccElement = document.querySelector('.js-create-acc-link');
 const createAccContainer = document.querySelector('.js-create-container');
+const forgotContainer = document.querySelector('.js-forgot-container');
+
+const createAccLink = document.querySelector('.js-create-acc-link');
+const forgotLink = document.querySelector('.js-forgot-link');
+const loginLink = document.querySelector('.js-sign-in-link');
 
 function switchToCreatePage() {
-  if (loginContainer.classList.contains('active')) {
-    loginContainer.classList.remove('active');
-    createAccContainer.classList.add('active');
-  } 
+  loginContainer.classList.remove('active');
+  forgotContainer.classList.remove('active');
+  createAccContainer.classList.add('active');
 }
 
-createAccElement.addEventListener('click', () => {
+function switchToLoginPage() {
+  loginContainer.classList.add('active');
+  forgotContainer.classList.remove('active');
+  createAccContainer.classList.remove('active');
+}
+
+function switchToForgotPage() {
+  loginContainer.classList.remove('active');
+  forgotContainer.classList.add('active');
+  createAccContainer.classList.remove('active');
+}
+
+createAccLink.addEventListener('click', () => {
   switchToCreatePage();
 });
 
+loginLink.addEventListener('click', () => {
+  switchToLoginPage();
+});
 
-
-
-
-
-
+forgotLink.addEventListener('click', () => {
+  switchToForgotPage();
+});
